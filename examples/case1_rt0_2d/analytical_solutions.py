268cb9c7ddcacf68c7962810f6d641bdbd5060db
wmboon <wietseboon@gmail.com>
2024-05-10 17:16:09 +0200

    transposed vector_gradient fixed matrix_div renamed asym and asym_T

--- examples/case1_rt0_2d/analytical_solutions.py
+++ examples/case1_rt0_2d/analytical_solutions.py
@@ -14,25 +14,21 @@ def to_numpy(expr, R, i):
 
 
 def vector_gradient(u, R):
-    return Matrix(
-        [diff(u[i], var) for var in R.varlist for i in range(len(u))]
-    ).reshape(3, 3)
+    return Matrix([diff(u_i, var) for u_i in u for var in R.varlist]).reshape(3, 3)
 
 
-def asym(r):
+def asym_T(r):
     return Matrix([[0, -r[2], r[1]], [r[2], 0, -r[0]], [-r[1], r[0], 0]])
 
 
-def asym_T(sigma):
+def asym(sigma):
     asym_sigma = sigma - sigma.T
     return Matrix([asym_sigma[2, 1], asym_sigma[0, 2], asym_sigma[1, 0]])
 
 
 def matrix_divergence(sigma, R):
     x, y, z = R.varlist
-    return Matrix(
-        [diff(sigma, x).trace(), diff(sigma, y).trace(), diff(sigma, z).trace()]
-    )
+    return diff(sigma[:, 0], x) + diff(sigma[:, 1], y) + diff(sigma[:, 2], z)
 
 
 ## -------------------------------------------------------------------##
@@ -44,8 +40,8 @@ def main():
     x, y, z = R.varlist
 
     # define the displacement
-    u_x = x  # sin(2 * pi * x) * sin(2 * pi * y)
-    u_y = y  # sin(2 * pi * x) * sin(2 * pi * y)
+    u_x = x * x - y  # sin(2 * pi * x) * sin(2 * pi * y)
+    u_y = x  # sin(2 * pi * x) * sin(2 * pi * y)
     u = Matrix([u_x, u_y, 0])
 
     # define the rotation
@@ -53,10 +49,10 @@ def main():
     r = Matrix([0, 0, r_z])
 
     # Stress tensor with, mu = 0.5 and lambda = 0
-    sigma = vector_gradient(u, R) + asym(r)
+    sigma = vector_gradient(u, R) + asym_T(r)
     print("sigma", sigma)
     print("grad u", vector_gradient(u, R))
-    print("asym r", asym(r))
+    print("asym r", asym_T(r))
 
     # Micro stress tensor with, mu = 0.5 and lambda = 0
     omega = vector_gradient(r, R)
@@ -66,11 +62,11 @@ def main():
     f_u = -matrix_divergence(sigma, R)
     print("f_u", f_u)
 
-    f_r = asym_T(sigma) - matrix_divergence(omega, R)
+    f_r = asym(sigma) - matrix_divergence(omega, R)
     print("f_r", f_r)
 
     # check the asym operator
-    print(asym_T(asym(r)) - 2 * r)
+    print(asym(asym_T(r)) - 2 * r)
 
     # print("velocity\n", to_numpy(u, R, 0))
     # print("velocity\n", to_numpy(u, R, 1))

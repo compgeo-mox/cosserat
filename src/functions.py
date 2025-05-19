8ad5f721f709cf84add0f4d947bcf8865f8c25a7
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2025-05-19 08:33:46 +0200

    reduce the number of simu

--- src/functions.py
+++ src/functions.py
@@ -27,7 +27,7 @@ def solve_not_lumped(dim, mesh_size, folder, setup, solver_class):
 
 def run_2d(func, folder, file_name, setup, solver_class):
     dim = 2
-    mesh_size = np.power(2.0, -np.arange(3, 3 + 5))
+    mesh_size = np.power(2.0, -np.arange(3, 3 + 4))  # 5 6
     errs = np.vstack([func(dim, h, folder, setup, solver_class) for h in mesh_size])
     errs_latex = make_summary(errs)
 
@@ -38,7 +38,7 @@ def run_2d(func, folder, file_name, setup, solver_class):
 
 def run_3d(func, folder, file_name, setup, solver_class):
     dim = 3
-    mesh_size = [0.4, 0.3, 0.2, 0.1]
+    mesh_size = [0.4, 0.3, 0.2]  # , 0.1]
     errs = np.vstack([func(dim, h, folder, setup, solver_class) for h in mesh_size])
     errs_latex = make_summary(errs)
 

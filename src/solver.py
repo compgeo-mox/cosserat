50ac563f0af9663f08798b4fe1a3b1782396f123
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2025-06-04 08:49:48 +0200

    update solver

--- src/solver.py
+++ src/solver.py
@@ -360,7 +360,7 @@ class SolverBDM1_L1(Solver):
         err_u = self.dis_u.error_l2(self.sd, u, u_ex)
         r_l2 = self.dis_r.proj_to_lagrange2(self.sd) @ r
         if self.dim == 2:
-            err_r = self.l2.error_l2(self.sd, r_l2, r_ex)
+            err_r = self.p2.error_l2(self.sd, r_l2, r_ex)
         else:
             err_r = self.vec_l2.error_l2(self.sd, r_l2, r_ex)
 
@@ -420,7 +420,7 @@ class SolverRT1_L1(Solver):
         if self.dim == 2:
             M_p2 = self.p2.assemble_lumped_matrix(self.sd)
             M_p1 = self.p1.assemble_mass_matrix(self.sd)
-            proj_l1_p2 = self.l2.proj_to_pwQuadratics(self.sd) @ proj_l1_p2
+            proj_l1_p2 = self.p2.proj_to_pwQuadratics(self.sd) @ proj_l1_p2
 
         else:
             M_p2 = self.vec_p2.assemble_lumped_matrix(self.sd)
@@ -446,7 +446,7 @@ class SolverRT1_L1(Solver):
         if self.dim == 2:
             M_p2 = self.p2.assemble_lumped_matrix(self.sd)
             r_interp = self.p2.interpolate(self.sd, f_r)
-            proj_l1_p2 = self.l2.proj_to_pwQuadratics(self.sd) @ proj_l1_p2
+            proj_l1_p2 = self.p2.proj_to_pwQuadratics(self.sd) @ proj_l1_p2
         else:
             M_p2 = self.vec_p2.assemble_lumped_matrix(self.sd)
             r_interp = self.vec_p2.interpolate(self.sd, f_r)
@@ -474,8 +474,84 @@ class SolverRT1_L1(Solver):
         err_u = self.dis_u.error_l2(self.sd, u, u_ex)
         r_l2 = self.dis_r.proj_to_lagrange2(self.sd) @ r
         if self.dim == 2:
-            err_r = self.l2.error_l2(self.sd, r_l2, r_ex)
+            err_r = self.p2.error_l2(self.sd, r_l2, r_ex)
         else:
             err_r = self.vec_l2.error_l2(self.sd, r_l2, r_ex)
 
         return err_s, err_w, err_u, err_r
+
+
+class SolverRT1_P1(Solver):
+    def create_family(self):
+        self.dis_s = self.vec_rt1
+        self.dis_w = self.rt1 if self.dim == 2 else self.vec_rt1
+        self.dis_u = self.vec_p1
+        self.dis_r = self.p1 if self.dim == 2 else self.vec_p1
+
+    def build_diff(self, M_u, M_r):
+        proj_p1_p2 = self.dis_r.proj_to_pwQuadratics(self.sd)
+
+        if self.dim == 2:
+            M_p2 = self.p2.assemble_mass_matrix(self.sd)
+            M_p1 = self.p1.assemble_mass_matrix(self.sd)
+
+        else:
+            M_p2 = self.vec_p2.assemble_mass_matrix(self.sd)
+            M_p1 = self.vec_p1.assemble_mass_matrix(self.sd)
+
+        div_s = M_u @ self.dis_s.assemble_diff_matrix(self.sd)
+
+        asym_op = self.dis_s.assemble_asym_matrix(self.sd)
+        asym = proj_p1_p2.T @ M_p2 @ asym_op
+
+        div_w_op = self.dis_w.assemble_diff_matrix(self.sd)
+        div_w = M_p1 @ div_w_op
+
+        return div_s, asym, div_w
+
+    def build_bc_for(self, M_u, M_r, data_pb):
+        f_u, f_r = data_pb["f_u"], data_pb["f_r"]
+
+        if self.dim == 2:
+            M_p1 = self.p1.assemble_mass_matrix(self.sd)
+            r_interp = self.p1.interpolate(self.sd, f_r)
+        else:
+            M_p1 = self.vec_p1.assemble_mass_matrix(self.sd)
+            r_interp = self.vec_p1.interpolate(self.sd, f_r)
+
+        # Assemble the source terms
+        u_for = M_u @ self.dis_u.interpolate(self.sd, f_u)
+        r_for = M_p1 @ r_interp
+
+        # Assemble the boundary conditions
+        u_ex, r_ex = data_pb["u_ex"], data_pb["r_ex"]
+        bd_faces = self.sd.tags["domain_boundary_faces"]
+        u_bc = self.dis_s.assemble_nat_bc(self.sd, u_ex, bd_faces)
+        r_bc = self.dis_w.assemble_nat_bc(self.sd, r_ex, bd_faces)
+
+        return u_bc, r_bc, u_for, r_for
+
+    def compute_err(self, s, w, u, r, data, data_pb):
+        # compute the error
+        s_ex, w_ex = data_pb["s_ex"], data_pb["w_ex"]
+        u_ex, r_ex = data_pb["u_ex"], data_pb["r_ex"]
+
+        err_s = self.dis_s.error_l2(self.sd, s, s_ex)
+        err_w = self.dis_w.error_l2(self.sd, w, w_ex)
+        err_u = self.dis_u.error_l2(self.sd, u, u_ex)
+
+        r_p2 = self.dis_r.proj_to_pwQuadratics(self.sd) @ r
+        if self.dim == 2:
+            err_r = self.p2.error_l2(self.sd, r_p2, r_ex)
+        else:
+            err_r = self.vec_p2.error_l2(self.sd, r_p2, r_ex)
+
+        return err_s, err_w, err_u, err_r
+
+    def create_grid(self, mesh_size, folder):
+        mesh_file_name = os.path.join(folder, "grid.msh")
+        self.sd = pg.unit_grid(
+            self.dim, mesh_size, as_mdg=False, file_name=mesh_file_name
+        )
+        self.sd = pg.barycentric_split(self.sd)
+        self.sd.compute_geometry()

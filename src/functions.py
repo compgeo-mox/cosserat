5b99f6f0ea4b646131a08e1f04d6a56594909739
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2025-07-15 13:30:28 +0200

    fix rhs for 3d and grid size

--- src/functions.py
+++ src/functions.py
@@ -40,7 +40,7 @@ def run_2d(func, folder, file_name, setup, solver_class):
 
 def run_3d(func, folder, file_name, setup, solver_class):
     dim = 3
-    mesh_size = [0.4, 0.3]  # , 0.2, 0.1]
+    mesh_size = [1 / 3, 2 / 9 , 4 / 27, 8 / 81]
     errs = np.vstack([func(dim, h, folder, setup, solver_class) for h in mesh_size])
     print(errs)
     errs_latex = make_summary(errs)
@@ -89,12 +89,16 @@ def setup(dim, alpha=0, beta=1):
         rhs = ss.rhs_scaled(param, dim)(*pt)
         if dim == 2:
             rhs = rhs[:2]
+        else:
+            rhs = rhs[:3]
         return rhs
 
     def rhs_scaled_r(pt):
         rhs = ss.rhs_scaled(param, dim)(*pt)
         if dim == 2:
             rhs = rhs[-1]
+        else:
+            rhs = rhs[-3:]
         return rhs
 
     data_pb = {
